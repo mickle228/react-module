677e6ba592c18e5360297131d10b52674de7aa39
mickle228 <olehkachan05@gmail.com>
2024-01-31 22:34:29 +0200

    lesson1-task1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import FirstComponent from "./components/first-component/FirstComponent";
 
-function App() {
-  return (
-    <div className="App">
-        <FirstComponent/>
-        <FirstComponent/>
-        <FirstComponent/>
-    </div>
-  );
-}
+import React from 'react';
+
+const App = () => {
+    const family = [
+        {
+            name: "Homer",
+            id: 1,
+            photo: "https://bit.ly/2z2rfVm"
+        },
+
+        {
+            name: "Marge",
+            id: 2,
+            photo: "https://bit.ly/2Krycoj"
+        },
+
+        {
+            name: "Bart",
+            id: 3,
+            photo: "https://bit.ly/2lNZrun"
+        },
+
+        {
+            name: "Lisa",
+            id: 4,
+            photo: "https://bit.ly/2tXaOEg"
+        },
+
+        {
+            name: "Maggie",
+            id: 5,
+            photo: "https://bit.ly/2IL7Tnj"
+        }
+    ];
+    return (
+        <div>
+            {family.map(person=> <FirstComponent person={person} key={person.id}/>)}
+        </div>
+    );
+};
 
 export default App;
--- src/components/first-component/FirstComponent.js
+++ src/components/first-component/FirstComponent.js
@@ -1,11 +1,10 @@
-import React from 'react';
+const FirstComponent = ({person}) => {
+    const {name, photo} = person;
 
-const FirstComponent = () => {
     return (
         <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAHI6188ZnI5y0kFIxP6IB5NLVYgFsWzv19GXnzyTB&s" alt=""/>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAHI6188ZnI5y0kFIxP6IB5NLVYgFsWzv19GXnzyTB&s" alt=""/>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAHI6188ZnI5y0kFIxP6IB5NLVYgFsWzv19GXnzyTB&s" alt=""/>
+            <h1>{name}</h1>
+            <img src={photo} alt={name}/>
         </div>
     );
 };
