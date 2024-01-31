89308785617fc61356c9ebfdbd1d8287068b96a4
mickle228 <olehkachan05@gmail.com>
2024-01-31 19:57:59 +0200

    lesson1

---
+++ src/components/first-component/FirstComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FirstComponent = () => {
+    return (
+        <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAHI6188ZnI5y0kFIxP6IB5NLVYgFsWzv19GXnzyTB&s" alt=""/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAHI6188ZnI5y0kFIxP6IB5NLVYgFsWzv19GXnzyTB&s" alt=""/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAHI6188ZnI5y0kFIxP6IB5NLVYgFsWzv19GXnzyTB&s" alt=""/>
+        </div>
+    );
+};
+
+export default FirstComponent;
