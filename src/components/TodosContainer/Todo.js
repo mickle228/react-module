cc6a07c3eca53cffb74d67d157931209c66a13fb
mickle228 <olehkachan05@gmail.com>
2024-02-13 19:02:08 +0200

    lesson4-fix

--- src/components/TodosContainer/Todo.js
+++ src/components/TodosContainer/Todo.js
@@ -6,7 +6,7 @@ const Todo = ({todo}) => {
         <div>
             <div>id: {id}</div>
             <div>title: {title}</div>
-            <div>completed: {completed}</div>
+            <div>completed: {completed.toString()}</div>
         </div>
     );
 };
